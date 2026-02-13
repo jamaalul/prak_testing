456538e0e58c7cf915d51e34f32078a4a9792416
jamaalul <a.jamaalul@gmail.com>
2026-02-14 05:15:11 +0700

    m

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -3,8 +3,10 @@
 @section('title', 'Dashboard - RSHP UNAIR')
 
 @section('content')
-    <div class="bg-white shadow-sm p-6 border rounded-lg">
-        <h3 class="mb-4 font-medium text-lg">Selamat Datang di Dashboard</h3>
-        <p class="text-gray-600">Anda berhasil masuk ke dashboard RSHP Universitas Airlangga.</p>
-    </div>
+    <h1 class="mt-6 text-2xl text-center">Selamat datang</h1>
+    <div class="h-96 overflow-hidden tenor-gif-embed" data-postid="11279902563780569961" data-share-method="host"
+        data-aspect-ratio="0.566265" data-width="100%"><a
+            href="https://tenor.com/view/cat-meow-360-cat-meme-gif-11279902563780569961">Cat Meow 360
+            GIF</a>from <a href="https://tenor.com/search/cat+meow-gifs">Cat Meow GIFs</a></div>
+    <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
 @endsection
